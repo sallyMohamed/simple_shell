ab1495b2754606faf6d280da5e6894326f210299
sarora2200 <sarora2200@gmail.com>
2023-08-23 12:37:36 -0700

    done

--- environment_func.c
+++ environment_func.c
@@ -3,54 +3,49 @@
 /**
  * _environment - Environment
  * @i: StructurArguments
- *
  * Return: Always 0
  */
 int _environment(i_t *i)
 {
-	say_stringlist(i->environ2);
-	return (0);
+say_stringlist(i->environ2);
+return (0);
 }
 
 /**
  * _envData - Environment variable
  * @i: StructurArguments
  * @env_name: env varAIABLE
- *
  * Return: the value
  */
 char *_envData(i_t *i, const char *env_name)
 {
-	list_str *nod_e = i->environ2;
-	char *pio;
-
-	while (nod_e)
-	{
-		pio = startWith(nod_e->string, env_name);
-		if (pio && *pio)
-			return (pio);
-		nod_e = nod_e->next;
-	}
-	return (NULL);
+list_str *nod_e = i->environ2;
+char *pio;
+while (nod_e)
+{
+pio = startWith(nod_e->string, env_name);
+if (pio && *pio)
+return (pio);
+nod_e = nod_e->next;
+}
+return (NULL);
 }
 
 /**
  * _setenvironment - InitializEnvironment variable
- *
  * @i: StructurArgumentS
- *
  *  Return: Always 0
  */
 int _setenvironment(i_t *i)
 {
-	if (i->argcounter != 3)
-	{
-		_inputputs("Error in arguements number\n");
-		return (1);
-	}
-	if (_setEnvo(i, i->argvactor[1], i->argvactor[2]))
-		return (0);
-	return (1);
+if (i->argcounter != 3)
+{
+_inputputs("Incorrect number of arguements\n");
+return (1);
+}
+if (_setEnvo(i, i->argvactor[1], i->argvactor[2]))
+return (0);
+return (1);
 }
 
 /**
@@ -60,17 +55,16 @@ int _setenvironment(i_t *i)
  */
 int _unsetenvironment(i_t *i)
 {
-	int c;
+int c;
+if (i->argcounter == 1)
+{
+_inputputs("Too few arguements.\n");
+return (1);
+}
+for (c = 1; c <= i->argcounter; c++)
+_unsetEnvo(i, i->argvactor[c]);
 
-	if (i->argcounter == 1)
-	{
-		_inputputs("little arguements.\n");
-		return (1);
-	}
-	for (c = 1; c <= i->argcounter; c++)
-		_unsetEnvo(i, i->argvactor[c]);
-
-	return (0);
+return (0);
 }
 
 /**
@@ -80,11 +74,10 @@ int _unsetenvironment(i_t *i)
  */
 int environment_list(i_t *i)
 {
-	list_str *nod_e = NULL;
-	size_t n;
-
-	for (n = 0; environ[n]; n++)
-		addto_end(&nod_e, environ[n], 0);
-	i->environ2 = nod_e;
-	return (0);
+list_str *nod_e = NULL;
+size_t n;
+for (n = 0; environ[n]; n++)
+addto_end(&nod_e, environ[n], 0);
+i->environ2 = nod_e;
+return (0);
 }
--- environment_func2.c
+++ environment_func2.c
@@ -3,90 +3,82 @@
 /**
  * **getEnvo - returns the string array copy of our environ
  * @i: StructureArguments.
- *
  * Return: Always 0
  */
 char **getEnvo(i_t *i)
 {
-	if (!i->environ || i->environ_changed)
-	{
-		i->environ = list_TOstr(i->environ2);
-		i->environ_changed = 0;
-	}
-
-	return (i->environ);
+if (!i->environ || i->environ_changed)
+{
+i->environ = list_TOstr(i->environ2);
+i->environ_changed = 0;
+}
+return (i->environ);
 }
 
 /**
  * _unsetEnvo - RemovEnvironment variable
  * @i: StructureArguments.
- *
- *  Return: 1 delete, 0 else
  * @variable: envVar String
+ * Return: 1 delete, 0 else
  */
 int _unsetEnvo(i_t *i, char *variable)
 {
-	list_str *nod_e = i->environ2;
-	size_t c = 0;
-	char *pio;
-
-	if (!nod_e || !variable)
-		return (0);
-
-	while (nod_e)
-	{
-		pio = startWith(nod_e->string, variable);
-		if (pio && *pio == '=')
-		{
-			i->environ_changed = _deletenodeatindex(&(i->environ2), c);
-			c = 0;
-			nod_e = i->environ2;
-			continue;
-		}
-		nod_e = nod_e->next;
-		c++;
-	}
-	return (i->environ_changed);
+list_str *nod_e = i->environ2;
+size_t c = 0;
+char *pio;
+if (!nod_e || !variable)
+return (0);
+while (nod_e)
+{
+pio = startWith(nod_e->string, variable);
+if (pio && *pio == '=')
+{
+i->environ_changed = _deletenodeatindex(&(i->environ2), c);
+c = 0;
+nod_e = i->environ2;
+continue;
+}
+nod_e = nod_e->next;
+c++;
+}
+return (i->environ_changed);
 }
 
 /**
  * _setEnvo - Environment variable,
- *
  * @i: StructureArguments.
- *
  * @variable: stringEnvVar
  * @vlu: value
- *  Return: Always 0
+ * Return: Always 0
  */
 int _setEnvo(i_t *i, char *variable, char *vlu)
 {
-	char *buffer = NULL;
-	list_str *nod_e;
-	char *pio;
-
-	if (!variable || !vlu)
-		return (0);
-	buffer = malloc(_stringlen(variable) + _stringlen(vlu) + 2);
-	if (!buffer)
-		return (1);
-	_strcpy(buffer, variable);
-	_stringcat2(buffer, "=");
-	_stringcat2(buffer, vlu);
-	nod_e = i->environ2;
-	while (nod_e)
-	{
-		pio = startWith(nod_e->string, variable);
-		if (pio && *pio == '=')
-		{
-			free(nod_e->string);
-			nod_e->string = buffer;
-			i->environ_changed = 1;
-			return (0);
-		}
-		nod_e = nod_e->next;
-	}
-	addto_end(&(i->environ2), buffer, 0);
-	free(buffer);
-	i->environ_changed = 1;
-	return (0);
+char *buffer = NULL;
+list_str *nod_e;
+char *pio;
+if (!variable || !vlu)
+return (0);
+buffer = malloc(_stringlen(variable) + _stringlen(vlu) + 2);
+if (!buffer)
+return (1);
+_strcpy(buffer, variable);
+_stringcat2(buffer, "=");
+_stringcat2(buffer, vlu);
+nod_e = i->environ2;
+while (nod_e)
+{
+pio = startWith(nod_e->string, variable);
+if (pio && *pio == '=')
+{
+free(nod_e->string);
+nod_e->string = buffer;
+i->environ_changed = 1;
+return (0);
+}
+nod_e = nod_e->next;
+}
+addto_end(&(i->environ2), buffer, 0);
+free(buffer);
+i->environ_changed = 1;
+return (0);
 }
